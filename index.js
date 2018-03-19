82241b78d6977639847491d579735696575b359b
Zhenmao <wanzhenmao@gmail.com>
2018-03-19 15:48:07 +0800

    feat: Add resize

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 	// Initial setup
 	const marginFocus = {top: 30, right: 70, bottom: 30, left: 70},
 				marginContext = {top: 20, right: marginFocus.right, bottom: 30, left: marginFocus.left},
-				width = $(".focus-chart").width() - marginFocus.left - marginFocus.right,
 				heightFocus = 400 - marginFocus.top - marginFocus.bottom,
 				heightContext = 100 - marginContext.top - marginContext.bottom,
 				tooltipPadding = 3;
+
+	let width = $(".focus-chart").width() - marginFocus.left - marginFocus.right;
 
 	const strokeColor = "#F79514",
 				strokeWidth = 2,
@@ -68,28 +69,34 @@
 
 	// SVG containers
 	const svgFocus = d3.select(".focus-chart").append("svg")
+			.attr("class", "focus-svg")
 			.attr("width", width + marginFocus.left + marginFocus.right)
 			.attr("height", heightFocus + marginFocus.top + marginFocus.bottom);
 
 	const gFocus = svgFocus.append("g")
+			.attr("class", "focus-g")
 			.attr("transform", `translate(${marginFocus.left},${marginFocus.top})`);
 
+	let tooltipContainer;
+
 	const svgContext = d3.select(".context-chart").append("svg")
+			.attr("class", "context-svg")
 			.attr("width", width + marginContext.left + marginContext.right)
 			.attr("heigth", heightContext + marginContext.top + marginContext.bottom);
 
 	const gContext = svgContext.append("g")
+			.attr("class", "context-g")
 			.attr("transform", `translate(${marginContext.left},${marginContext.top})`);
 
 	// Clip path
-	svgFocus.append("defs")
+	const svgFocusClipPath = svgFocus.append("defs")
 		.append("clipPath")
 			.attr("id", "clip-path-focus")
 		.append("rect")
 			.attr("width", width)
 			.attr("height", heightFocus);
 
-	svgContext.append("defs")
+	const svgContextClipPath = svgContext.append("defs")
 		.append("clipPath")
 			.attr("id", "clip-path-context")
 		.append("rect")
@@ -109,6 +116,9 @@
 	});
 
 	function renderContext() {
+		gContext.selectAll("*").remove();
+		svgContext.select(".brush").remove();
+
 		xContext.domain(d3.extent(data, d => d.date));
 		yContext.domain(d3.extent(data, d => d.close));
 
@@ -171,6 +181,8 @@
 	}
 
 	function renderFocus() {
+		gFocus.selectAll("*").remove();
+
 		xFocus.domain(d3.extent(data, d => d.date));
 		yFocus.domain(d3.extent(data, d => d.close));
 
@@ -398,5 +410,47 @@
 				"V" + (2 * y - 8) +
 				"M" + (4.5 * x) + "," + (y + 8) +
 				"V" + (2 * y - 8);
+	}
+
+	// Optimized resize
+	// https://developer.mozilla.org/en-US/docs/Web/Events/resize
+	(function () {
+		var throttle = function (type, name, obj) {
+			obj = obj || window;
+			var running = false;
+			var func = function () {
+				if (running) { return; }
+				running = true;
+				requestAnimationFrame(function () {
+					obj.dispatchEvent(new CustomEvent(name));
+					running = false;
+				});
+			};
+			obj.addEventListener(type, func);
+		};
+
+		/* init - you can init any event */
+		throttle("resize", "optimizedResize");
+	})();
+
+	// Listen for resize changes
+	window.addEventListener("optimizedResize", reDraw, false);
+	// Listen for orientation changes
+	window.addEventListener("orientationchange", reDraw, false);
+
+	function reDraw() {
+		width = $(".focus-chart").width() - marginFocus.left - marginFocus.right;
+		brush.extent([[0, 0], [width, heightContext]]);
+		zoom.translateExtent([[0, 0], [width, heightFocus]])
+				.extent([[0, 0], [width, heightFocus]]);
+		xFocus.range([0, width]);
+		xContext.range([0, width]);
+		svgFocus.attr("width", width + marginFocus.left + marginFocus.right);
+		svgContext.attr("width", width + marginContext.left + marginContext.right);
+		svgFocusClipPath.attr("width", width);
+		svgContextClipPath.attr("width", width);
+		renderContext();
+		renderFocus();
+		renderTooltip();
 	}
 })();
